5633aedf9bba70e4d508005f42813b7f4804c643
Krystian Koszucki <krystiankoszucki@wp.pl>
2023-09-15 20:15:35 +0200

    Seeding database roles' table #02

--- GymSeeder.cs
+++ GymSeeder.cs
@@ -15,6 +15,14 @@ namespace GymAPI
         {
             if(_dbContext.Database.CanConnect())
             {
+                if(!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+
+
                 if(!_dbContext.Trainings.Any())
                 {
                     var trainings = GetTrainings();
@@ -23,6 +31,26 @@ namespace GymAPI
                 }
             }
 
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name= "User"
+                },
+                new Role()
+                {
+                    Name= "Manager"
+                },
+                new Role()
+                {
+                    Name= "Admin"
+                },
+            };
+            return roles;
         }
 
         private IEnumerable<Training> GetTrainings()
